2ab3c1074a62d4da6ec09a98fd06ca611f0dfc4d
Vishnukant Biradar <vishnubiradar5148@gmail.com>
2025-11-30 11:01:33 +0530

    vishnu30

--- lecture_eight.py
+++ lecture_eight.py
@@ -1,23 +1,10 @@
-class Account:
-    def __init__(self, bal, acc):
-        self.balance = bal
-        self.account_no = acc
+class student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+        self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
 
-    def debit(self, amount):
-        self.balance -= amount
-        print("Rs.", amount, "was debited")
-        print("total blance =", self.get_balance())
+stu1 = student(98, 97, 99)
+print(stu1.percentage)        
 
-    def credit(self, amount):
-        self.balance += amount
-        print("Rs.", amount, "was credited")
-        print("total balance =", self.get_balance())     
-       
-    def get_balance(self):
-        return self.balance   
-
-
-acc1 = Account(10000, 12345)
-acc1.debit(1000)
-acc1.credit(500)
-acc1.credit(25000)
