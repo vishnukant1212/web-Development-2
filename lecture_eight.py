51fec128e1c8b235ad5cf196a95aa10638f497bc
Vishnukant Biradar <vishnubiradar5148@gmail.com>
2025-11-30 11:05:57 +0530

    vishnu22

--- lecture_eight.py
+++ lecture_eight.py
@@ -1,10 +1,2 @@
-class student:
-    def __init__(self, phy, chem, math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-        self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
-
-stu1 = student(98, 97, 99)
-print(stu1.percentage)        
-
+name = input("What is your name? ")
+print("Hello, " + name + "!")
